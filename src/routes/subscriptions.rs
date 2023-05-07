d8cea6ecf890a71937152d7f847d0e3cfa0cb894
Aaron Zhang <aaron@Aarons-MacBook-Pro.local>
2023-05-07 12:05:00 +0800

    split lib.rs into multiple sub-modules

---
+++ src/routes/subscriptions.rs
@@ -0,0 +1,13 @@
+use actix_web::{web, HttpResponse};
+
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    email: String,
+    name: String,
+}
+
+// All arguments in the signature of a route handler must implement the FromRequest trait
+// actix-web will invoke from_request for each argument and run the actual handler function.
+pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}

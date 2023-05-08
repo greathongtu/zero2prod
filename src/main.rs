e4b7b7dcca352dc211610ffea2df8cafd2b89aea
Aaron Zhang <aaron@Aarons-MacBook-Pro.local>
2023-05-08 21:21:02 +0800

    finish deployment chapter

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use sqlx::PgPool;
 use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
@@ -11,13 +10,9 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect_lazy(&configuration.database.connection_string())
-        .expect("Failed to connect to Postgres.");
-    // let connection_pool = PgPoolOptions::new()
-    //     .acquire_timeout(std::time::Duration::from_secs(2))
-    //     .connect(&configuration.database.connection_string())
-    //     .await
-    //     .expect("Failed to connect to Postgres.");
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(configuration.database.with_db());
 
     let address = format!("{}:{}", configuration.application.host, configuration.application.port);
     let listener = TcpListener::bind(address)?;

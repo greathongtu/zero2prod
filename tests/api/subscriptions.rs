54b2080ed2743a94de649112aa801c34f0bf60a7
Aaron Zhang <aaron@Aarons-MacBook-Pro.local>
2023-05-11 15:47:46 +0800

    finished 7.6.2

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -75,3 +75,41 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
         );
     }
 }
+
+#[tokio::test]
+async fn subscribe_sends_a_confirmation_email_with_a_link() {
+    // Arrange
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+  
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        // We are not setting an expectation here anymore
+        // The test is focused on another aspect of the app
+        // behaviour.    
+        .mount(&app.email_server)
+        .await;
+  
+    // Act
+    app.post_subscriptions(body.into()).await;
+  
+    // Assert
+    // Get the first intercepted request
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+    let body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
+    let get_link = |s: &str| {
+        // Get the links from the email body
+        let links: Vec<_> = linkify::LinkFinder::new()
+            .links(s)
+            .filter(|l| *l.kind() == linkify::LinkKind::Url)
+            .collect();
+        // Get the first link
+        assert_eq!(links.len(), 1);
+        links[0].as_str().to_owned()
+    };
+    let html_link = get_link(body["HtmlBody"].as_str().unwrap());
+    let text_link = get_link(body["TextBody"].as_str().unwrap());
+
+    assert_eq!(html_link, text_link);
+}

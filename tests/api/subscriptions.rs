e8c5cd7f764f1dd701c08c5cba5ee01b579dca31
Aaron Zhang <aaron@Aarons-MBP.lan>
2023-05-12 22:54:46 +0800

    finished chapter 8: error handling

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -9,31 +9,97 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
 
     Mock::given(path("/email"))
-    .and(method("POST"))
-    .respond_with(ResponseTemplate::new(200))
-    .mount(&app.email_server)
-    .await;
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
 
     // Act
     let response = app.post_subscriptions(body.into()).await;
+
     // Assert
     assert_eq!(200, response.status().as_u16());
 }
 
 #[tokio::test]
 async fn subscribe_persists_the_new_subscriber() {
+    // Arrange
     let app = spawn_app().await;
-
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    // Act
     app.post_subscriptions(body.into()).await;
 
-    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+    // Assert
+    let saved = sqlx::query!("SELECT email, name, status FROM subscriptions",)
         .fetch_one(&app.db_pool)
         .await
         .expect("Failed to fetch saved subscription.");
 
     assert_eq!(saved.email, "ursula_le_guin@gmail.com");
     assert_eq!(saved.name, "le guin");
+    assert_eq!(saved.status, "pending_confirmation");
+}
+
+#[tokio::test]
+async fn subscribe_fails_if_there_is_a_fatal_database_error() {
+    // Arrange
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    // Sabotage the database
+    sqlx::query!("ALTER TABLE subscriptions DROP COLUMN email;",)
+        .execute(&app.db_pool)
+        .await
+        .unwrap();
+
+    // Act
+    let response = app.post_subscriptions(body.into()).await;
+
+    // Assert
+    assert_eq!(response.status().as_u16(), 500);
+}
+
+#[tokio::test]
+async fn subscribe_sends_a_confirmation_email_for_valid_data() {
+    // Arrange
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    // Act
+    app.post_subscriptions(body.into()).await;
+
+    // Assert
+    // Mock asserts on drop
+}
+
+#[tokio::test]
+async fn subscribe_sends_a_confirmation_email_with_a_link() {
+    // Arrange
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    // Act
+    app.post_subscriptions(body.into()).await;
+
+    // Assert
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+    let confirmation_links = app.get_confirmation_links(email_request);
+
+    // The two links should be identical
+    assert_eq!(confirmation_links.html, confirmation_links.plain_text);
 }
 
 #[tokio::test]
@@ -61,7 +127,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
     }
 }
 
-#[actix_web::test]
+#[tokio::test]
 async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
     // Arrange
     let app = spawn_app().await;
@@ -74,6 +140,7 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
     for (body, description) in test_cases {
         // Act
         let response = app.post_subscriptions(body.into()).await;
+
         // Assert
         assert_eq!(
             400,
@@ -82,30 +149,4 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
             description
         );
     }
-}
-
-#[tokio::test]
-async fn subscribe_sends_a_confirmation_email_with_a_link() {
-    // Arrange
-    let app = spawn_app().await;
-    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
-
-    Mock::given(path("/email"))
-        .and(method("POST"))
-        .respond_with(ResponseTemplate::new(200))
-        // We are not setting an expectation here anymore
-        // The test is focused on another aspect of the app
-        // behaviour.
-        .mount(&app.email_server)
-        .await;
-
-    // Act
-    app.post_subscriptions(body.into()).await;
-
-    // Assert
-    // Get the first intercepted request
-    let email_request = &app.email_server.received_requests().await.unwrap()[0];
-
-    let confirmation_links = app.get_confirmation_links(&email_request);
-    assert_eq!(confirmation_links.html, confirmation_links.plain_text);
 }
